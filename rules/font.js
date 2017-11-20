9a3e2c6f0285a733e9a086f63843b56c219a5f2f
Oleg Pimenov <opimenov@ptsecurity.com>
2017-11-20 06:02:44 +0300

    added font rules

---
+++ rules/font.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    // Disallow duplicate font family names.
+    'font-family-no-duplicate-names': true,
+
+    // Require numeric or named (where possible) font-weight values.
+    'font-weight-notation': 'numeric',
+};
